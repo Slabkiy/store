54a5af7f2d6aff07a34970cd070acc6fbb204657
Slabkii <slabkiya@gmail.com>
2019-10-22 11:26:40 +0300

    build

---
+++ client/build/precache-manifest.1f3a9d25a584438b63cf5fed0e2e02a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14742067bf158b1a40d6644fc741d0b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4128dd514cc49cb7c25d",
+    "url": "/static/css/main.ab0d20f2.chunk.css"
+  },
+  {
+    "revision": "8a645a0d68f3a5034b7b",
+    "url": "/static/js/2.9b781a9f.chunk.js"
+  },
+  {
+    "revision": "4128dd514cc49cb7c25d",
+    "url": "/static/js/main.f46e9819.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
